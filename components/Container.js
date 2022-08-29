db162c1454b943cd81920ec14c06304fc4215a76
Nicholas <nnlaor@gmail.com>
2022-08-29 00:37:42 -0400

    feat: working on the conainer that most of the other components will go into

---
+++ components/Container.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Head from "next/head";
+import { useRouter } from "next/router"
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+export default function Container({ children, ...customMeta}){
+    const router = useRouter();
+
+    const meta = {
+        title: "Nicholas Noochla-or - Developer",
+        description: "Coding out from the IT trenches.",
+        image: "/avatar.png",
+        type: "webstie",
+        ...customMeta,
+    };
+    return(
+        <div>
+            <Head>
+                <title>{meta.title}</title>
+            </Head>
+            <main className="dark:bg-greay-800 w-full">
+                <Nav />
+                <div>{children}</div>
+                <Footer />
+            </main>
+        </div>
+    );
+}
