7bad37707f7a54075fe4926312777d6dcee8355a
Nicholas Noochla-or <nnlaor@gmail.com>
2022-08-31 03:25:52 -0400

    finally got something on screen

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Container from "../components/Container";
 // import Hero from "../components/Hero";
+import userData from "@constants/data";
 
 
 export default function Home() {
