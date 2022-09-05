fc132bf902a173962abe470a4a956b1ba04712b7
Nicholas <nnlaor@gmail.com>
2022-09-05 11:20:44 -0400

    adding nav changes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
 import Container from "../components/Container";
-// import Hero from "../components/Hero";
 import userData from "@constants/data";
 
 
