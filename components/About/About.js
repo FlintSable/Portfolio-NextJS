531376e803b6afe818f7c7ba40db8efd643822a7
Nicholas Noochla-or <nnlaor@gmail.com>
2024-10-15 14:36:54 -0400

    changed the grid row heights to mostly auto bc of overflowing

--- components/About/About.js
+++ components/About/About.js
@@ -4,44 +4,52 @@ import styles from "./About.module.css";
 
 import Image from "next/image";
 
+const aboutContent = {
+  paragraphs: [
+    "Hi! I'm a developer based in Los Angeles, CA interested in working with web technologies, especially at the cross section of Front-end and Back-end engineering. My primary education is in Graphic Arts. I have a background in fixing desktops, laptops, servers and scripts mainly in CAD and 3D modeling design studio environments.",
+    "His ad iudico suavitate. Duo augue noster appareat ad. Aliquip oblique sed te. Mea tractatos repudiandae no. Modo mutat malorum ad est. Cu sint mollis neglegentur pri, inermis disputationi ne quo. Nam id blandit argumentum, sit paulo simul no."
+  ],
+  skills: [
+    "JavaScript",
+    "Python",
+    "React",
+    "Node.js",
+    "HTML/CSS",
+    "Git",
+    "SQL",
+    "RESTful APIs"
+  ]
+};
+
+
 const About = () => {
   return (
     <div id="section-about" className={styles["main-about"]}>
-      <div className={"section-interior"}>
-        <h1 className={"section-h1"}>01. About</h1>
+      <div className={styles["section-interior"]}>
+        <h1 className={styles["section-h1"]}>01. About</h1>
         <div className={styles["layout-about"]}>
           <div className={styles["textbox-about"]}>
-            <p className="text-p-about">
-              Hi! I'm a developer based in Los Angeles, CA interested in working
-              with web technologies, expecially at the cross section of
-              Front-end and Back-end engineering. My primary education is in
-              Graphic Arts. I have a background in fixing desktops, laptops,
-              servers and scripts mainly in CAD and 3D modeling design studio
-              environments.
-              <br />
-              <br />
-              His ad iudico suavitate. Duo augue noster appareat ad. Aliquip
-              oblique sed te. Mea tractatos repudiandae no. Modo mutat malorum
-              ad est. Cu sint mollis neglegentur pri, inermis disputationi ne
-              quo. Nam id blandit argumentum, sit paulo simul no.
-            </p>
+            <p>{aboutContent.paragraphs[0]}</p>
+            <div className={styles["paragraph-with-image"]}>
+              <img
+                className={styles["inline-img"]}
+                src="/nic.png"
+                alt="Nicholas Noochla-or"
+              />
+              <p>{aboutContent.paragraphs[1]}</p>
+            </div>
             <ul className={styles["ul-about"]}>
-              <li>JavaScript</li>
-              <li>Python</li>
-              <li>React</li>
-              <li>four</li>
-              <li>five</li>
-              <li>six</li>
-              <li>seven</li>
-              <li>eight</li>
+              {aboutContent.skills.map((skill, index) => (
+                <li key={index}>{skill}</li>
+              ))}
             </ul>
           </div>
           <div className={styles["img-about"]}>
             <img
               className={styles["custom-img"]}
               src="/nic.png"
-              width="100%"
-            ></img>
+              alt="Nicholas Noochla-or"
+            />
           </div>
         </div>
       </div>
@@ -49,4 +57,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default About
